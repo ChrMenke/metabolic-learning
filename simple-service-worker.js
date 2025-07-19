1af50257fcf2cf804c4389779575a7ecf09d6c33
ChrMenke <menke.christian@mh-hannover.de>
2025-07-19 10:05:22 +0200

    Update simple-service-worker.js

--- simple-service-worker.js
+++ simple-service-worker.js
@@ -1,53 +1,54 @@
 // simple-service-worker.js
 const CACHE_NAME = 'metabolic-learning-v1';
 const urlsToCache = [
-  './',
-  './Master.html',
-  './simple-sync.js',
-  './manifest.json',
+  '/metabolic-learning/',
+  '/metabolic-learning/Master.html',
+  '/metabolic-learning/simple-sync.js',
+  '/metabolic-learning/manifest.json',
+  '/metabolic-learning/icons/icon-192x192.png',
   // Module (alphabetisch sortiert)
-  './aminosaeurien_teil1.html',
-  './aminosaeurien_teil2.html',
-  './carnitin_transport_teil1.html',
-  './carnitin_transport_teil2.html',
-  './cholesterol_steroid_biosynthesis_part1.html',
-  './cholesterol_steroid_biosynthesis_part2.html',
-  './cholesterol_steroid_biosynthesis_part2a.html',
-  './cholesterol_steroid_biosynthesis_part2b.html',
-  './cholesterol_steroid_biosynthesis_part3.html',
-  './fabry_lernmodul_teil1.html',
-  './fabry_lernmodul_teil2.html',
-  './fabry_lernmodul_teil3.html',
-  './fabry_lernmodul_teil4.html',
-  './glutarazid_modul_1.html',
-  './homocystinurie_lerneinheit.html',
-  './konfirmationsdiagnostik_seite1.html',
-  './konfirmationsdiagnostik_seite2.html',
-  './konfirmationsdiagnostik_seite3.html',
-  './konfirmationsdiagnostik_seite4.html',
-  './lysosomal_storage_part1.html',
-  './lysosomal_storage_part2.html',
-  './lysosomal_storage_part3.html',
-  './mitochondriopathien_teil1.html',
-  './mitochondriopathien_teil2.html',
-  './mitochondriopathien_teil3.html',
-  './neurotransmitter_teil1.html',
-  './neurotransmitter_teil2.html',
-  './neurotransmitter_teil3.html',
-  './organoacidaemien_teil1.html',
-  './organoacidaemien_teil2.html',
-  './organoacidaemien_teil3.html',
-  './peroxisomal_disorders_part1.html',
-  './peroxisomal_disorders_part2.html',
-  './peroxisomal_disorders_part3.html',
-  './purin_pyrimidin_lerneinheit.html',
-  './tyrosinaemie_teil1.html',
-  './tyrosinaemie_teil2.html',
-  './ucd_learning_module.html',
-  './ucd_learning_module_part1.html',
-  './ucd_learning_module_part2.html',
-  './ucd_learning_module_part3.html',
-  './vitamin_cofactor_defects.html'
+  '/metabolic-learning/aminosaeurien_teil1.html',
+  '/metabolic-learning/aminosaeurien_teil2.html',
+  '/metabolic-learning/carnitin_transport_teil1.html',
+  '/metabolic-learning/carnitin_transport_teil2.html',
+  '/metabolic-learning/cholesterol_steroid_biosynthesis_part1.html',
+  '/metabolic-learning/cholesterol_steroid_biosynthesis_part2.html',
+  '/metabolic-learning/cholesterol_steroid_biosynthesis_part2a.html',
+  '/metabolic-learning/cholesterol_steroid_biosynthesis_part2b.html',
+  '/metabolic-learning/cholesterol_steroid_biosynthesis_part3.html',
+  '/metabolic-learning/fabry_lernmodul_teil1.html',
+  '/metabolic-learning/fabry_lernmodul_teil2.html',
+  '/metabolic-learning/fabry_lernmodul_teil3.html',
+  '/metabolic-learning/fabry_lernmodul_teil4.html',
+  '/metabolic-learning/glutarazid_modul_1.html',
+  '/metabolic-learning/homocystinurie_lerneinheit.html',
+  '/metabolic-learning/konfirmationsdiagnostik_seite1.html',
+  '/metabolic-learning/konfirmationsdiagnostik_seite2.html',
+  '/metabolic-learning/konfirmationsdiagnostik_seite3.html',
+  '/metabolic-learning/konfirmationsdiagnostik_seite4.html',
+  '/metabolic-learning/lysosomal_storage_part1.html',
+  '/metabolic-learning/lysosomal_storage_part2.html',
+  '/metabolic-learning/lysosomal_storage_part3.html',
+  '/metabolic-learning/mitochondriopathien_teil1.html',
+  '/metabolic-learning/mitochondriopathien_teil2.html',
+  '/metabolic-learning/mitochondriopathien_teil3.html',
+  '/metabolic-learning/neurotransmitter_teil1.html',
+  '/metabolic-learning/neurotransmitter_teil2.html',
+  '/metabolic-learning/neurotransmitter_teil3.html',
+  '/metabolic-learning/organoacidaemien_teil1.html',
+  '/metabolic-learning/organoacidaemien_teil2.html',
+  '/metabolic-learning/organoacidaemien_teil3.html',
+  '/metabolic-learning/peroxisomal_disorders_part1.html',
+  '/metabolic-learning/peroxisomal_disorders_part2.html',
+  '/metabolic-learning/peroxisomal_disorders_part3.html',
+  '/metabolic-learning/purin_pyrimidin_lerneinheit.html',
+  '/metabolic-learning/tyrosinaemie_teil1.html',
+  '/metabolic-learning/tyrosinaemie_teil2.html',
+  '/metabolic-learning/ucd_learning_module.html',
+  '/metabolic-learning/ucd_learning_module_part1.html',
+  '/metabolic-learning/ucd_learning_module_part2.html',
+  '/metabolic-learning/ucd_learning_module_part3.html',
+  '/metabolic-learning/vitamin_cofactor_defects.html'
 ];
 
 // Installation
@@ -57,11 +58,11 @@ self.addEventListener('install', event => {
       .then(cache => {
         console.log('Cache geöffnet');
         return Promise.all(
-          urlsToCache.map(url => {
-            return cache.add(url).catch(err => {
-              console.error('Fehler beim Cachen von:', url, err);
-            });
-          })
+          urlsToCache.map(url =>
+            cache.add(url).catch(err =>
+              console.error('Fehler beim Cachen von:', url, err)
+            )
+          )
         );
       })
   );
@@ -70,15 +71,15 @@ self.addEventListener('install', event => {
 // Aktivierung
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
+    caches.keys().then(cacheNames =>
+      Promise.all(
         cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
-      );
-    })
+      )
+    )
   );
 });
 
@@ -86,8 +87,6 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
